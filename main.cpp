2e17a36b50fd2ae039f45139da604432af821ebc
InvestedBrick <julianbrecht25@gmail.com>
2024-05-25 22:21:24 +0200

    Added tests for bst

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include "dynamic_array.hpp"
 #include "hashmap.hpp"
 #include "queue.hpp"
-struct test{
-    int x;
-};
-void list_test(){
+#include "binary_search_tree.hpp"
+
+void test_list(){
+    std::cout << "LIST TEST START" << std::endl;
     List<int> my_list;
     my_list.append(4);
     my_list.append(2);
@@ -29,10 +29,11 @@ void list_test(){
     for (size_t i = 0; i < my_list.get_size(); i++){
         std::cout << my_list[i] << std::endl;
     }
-    std::cout << "Finished" << std::endl;
+    std::cout << "LIST TEST END" << std::endl;
 }
 
 void test_hashmap(){
+    std::cout << "HASHMAP TEST START" << std::endl;
     static_hashmap<std::string,int> map("",10);
     map.insert("test",3);
     std::cout << map.retrieve("test") << std::endl;
@@ -43,10 +44,11 @@ void test_hashmap(){
     map.insert("Hello World", 23);
     map.insert("Hello World",map.retrieve("Hello World") + 1);
     std::cout << map["Hello World"] << std::endl;
-    std::cout << "Finished" << std::endl;
+    std::cout << "HASHMAP TEST END" << std::endl;
 }
 
 void test_queue(){
+    std::cout << "QUEUE TEST START" << std::endl;
     Queue<int> my_queue;
     my_queue.push_back(3);
     my_queue.push_back(6);
@@ -59,13 +61,52 @@ void test_queue(){
     std::cout << "Size: " << my_queue.get_size() << " Capacity: " << my_queue.get_capacity() << std::endl;
     
     std::cout << my_queue.front() << std::endl;
-    std::cout << "Finished" << std::endl;
+    std::cout << "QUEUE TEST END" << std::endl;
 }
 
+void test_bst(){
+    std::cout << "BINRARY SEARCH TREE TEST START" << std::endl;
+    BST tree;
+    tree.add_node(12); //               12
+    tree.add_node(4);  //              /   |
+    tree.add_node(3);  //            4      19         (cant use backslash bc it comments out next line)
+    tree.add_node(11); //           /  |   
+    tree.add_node(19); //         3    11  
+
+
+    
+
+    auto lst = tree.get_elements_in_order();
+    for(size_t i = 0; i < lst.get_size(); i++){
+        std::cout << lst[i] << " " << std::endl;
+    }
+    tree.remove_node(4);
+    node* node = tree.get_node(12);
+    std::cout << "Node " << node->data << std::endl;
+    if(node->left == nullptr){
+        std::cout << "LEFT : NULL" << std::endl;
+    }else{
+        std::cout << "LEFT : " << node->left->data << std::endl;
+    }
+
+    if(node->right == nullptr){
+        std::cout << "RIGHT : NULL" << std::endl;
+    }else{
+        std::cout << "RIGHT : " << node->right->data << std::endl;
+    }
+    std::cout << "--------------------" << std::endl;
+    auto list = tree.get_elements_in_order();
+    for(size_t i = 0; i < list.get_size(); i++){
+        std::cout << list[i] << " " << std::endl;
+    }
+ 
+    std::cout << "BINRARY SEARCH TREE TEST END" << std::endl;
+}
 int main(void){
     
-    //list_test(); 
-    //test_hashmap();
+    test_list(); 
+    test_hashmap();
     test_queue();
+    test_bst();
     return 0;
 }
